edd59bc66a08e5cd7398d7f02c1f9de8d029bb1d
CRUSH04 <msng0408@naver.com>
2022-06-16 12:16:47 +0900

    Update

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,9 +1,13 @@
 # 파이썬의 기본 내장 함수가 아닌 다른 함수 혹은 다른 기능이 필요할 때 사용함
+from asyncio import events
+from email import message
 from pydoc import describe
+from random import randint
 from turtle import color, title
 import discord, asyncio
 from discord.utils import get
 from discord.ext import commands
+from discord import DMChannel
 import os;
 
 os.environ['DISCORD_BOT_TOKEN'] = "OTg1ODQ5MTI5NTgwMjU3Mjkw.G9RJwW.vh4hBjWdDHvey6_wp7zMevDyYd8ywLTsbDTIUE"
@@ -18,27 +22,50 @@ async def on_ready(): # 봇이 실행되면 한 번 실행됨 Unity Start
 
 @client.event
 async def on_message(message):
+    if str(message.channel.type) == "private" and message.author.id != 985849129580257290:
+            author = await client.get_user(961454870328655872).create_dm()
+            await author.send(str(message.author.name) + "(" + message.author.id + ") : " + message.content)
     message_context = message.content
     if(message_context.find("테스트") >= 0):
         await message.channel.send ("{} | {}, Hello".format(message.author, message.author.mention))
         await message.author.send ("{} | {}, User, Hello".format(message.author, message.author.mention))
-    if(message_context.find("집합") >= 0):
-        await message.channel.send ("모여라 !! @everyone")
+    
     await client.process_commands(message) # 메세지 중 명령어가 있을 경우 처리해주는 코드
 
 @client.command()
 async def 도움말(ctx):
     embed = discord.Embed(title="도움말",describe=f"명령어들", color=0x0080FF)
-    embed.add_field(name="~테스트",value=f"형식 : {client.command_prefix}테스트",inline=True)
-    embed.add_field(name="~집합",value=f"형식 : {client.command_prefix}집합",inline=False)
+    embed.add_field(name="테스트하기",value=f"형식 : {client.command_prefix}테스트",inline=True)
+    embed.add_field(name="집합시키기",value=f"형식 : {client.command_prefix}집합",inline=False)
+    embed.add_field(name="랜텀 팀",value=f"형식 : {client.command_prefix}랜덤 a b",inline=False)
     await ctx.send(embed = embed)
     return
 
+@client.command()
+async def 랜덤(ctx, member1="", member2=""):
+    embed = discord.Embed(title="랜덤",describe=f"결과", color=0x0080FF)
+    randomList = [member1,member2]
+    if(randint(0,1) == 0):
+        embed.add_field(name="결과",value=f"1팀 : {member1}\n2팀 :{member2}",inline=False)
+    else:
+        embed.add_field(name="결과",value=f"1팀 : {member2}\n2팀 :{member1}",inline=False)
+    await ctx.send(embed = embed)
+    return
+
+@client.command()
+async def 팀(ctx):
+    embed = discord.Embed(title="팀 추첨 결과",describe=f"결과", color=0x0080FF)
+    
+    embed.add_field(name="1팀",value=f"[TANKER]김범주\n [BRAIN]이민형\n [SUB TANKER]유지호\n [NORMAL]김홍열",inline=False)
+    embed.add_field(name="2팀",value=f"[TANKER]김건범\n [BRAIN]이상백\n [SUB TANKER]박광현\n [NORMAL]이승혁",inline=False)
+    await ctx.send(embed = embed)
+    return
     
 @client.command()
-@commands.dm_only()
-async def on_message(message):
-    await message.author.send ("말 걸지 말아주세요 !!")
+async def 집합(ctx):
+    await ctx.channel.send ("모여라 !! @everyone")
+    return ctx
+
 
     
 
