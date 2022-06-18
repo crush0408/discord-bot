58061726d1efc73e47847053f29af4a85f1d06d8
CRUSH04 <msng0408@naver.com>
2022-06-19 04:05:34 +0900

    Update

--- Untitled-1.py
+++ Untitled-1.py
@@ -2,7 +2,7 @@
 from asyncio import events
 from email import message
 from pydoc import describe
-from random import randint
+from random import randint, random
 from turtle import color, title
 import discord, asyncio
 from discord.utils import get
@@ -34,19 +34,19 @@ async def on_message(message):
 async def 도움말(ctx):
     embed = discord.Embed(title="도움말",describe=f"명령어들", color=0x0080FF)
     embed.add_field(name="테스트하기",value=f"형식 : {client.command_prefix}테스트",inline=True)
-    embed.add_field(name="집합시키기",value=f"형식 : {client.command_prefix}집합",inline=False)
+    embed.add_field(name="집합시키기",value=f"형식 : {client.command_prefix}집합 보내고싶은 메세지",inline=False)
     embed.add_field(name="랜텀 팀",value=f"형식 : {client.command_prefix}랜덤 a b",inline=False)
     await ctx.send(embed = embed)
     return
 
 @client.command()
-async def 랜덤(ctx, member1="", member2=""):
+async def 랜덤(ctx, member1, member2):
     embed = discord.Embed(title="랜덤",describe=f"결과", color=0x0080FF)
-    randomList = [member1,member2]
     if(randint(0,1) == 0):
-        embed.add_field(name="결과",value=f"1팀 : {member1}\n2팀 :{member2}",inline=False)
+        embed.add_field(name="결과",value=f"{member1}",inline=False)
     else:
-        embed.add_field(name="결과",value=f"1팀 : {member2}\n2팀 :{member1}",inline=False)
+        embed.add_field(name="결과",value=f"{member2}",inline=False)
+    
     await ctx.send(embed = embed)
     return
 
@@ -54,15 +54,17 @@ async def 랜덤(ctx, member1="", member2=""):
 async def 팀(ctx):
     embed = discord.Embed(title="팀 추첨 결과",describe=f"결과", color=0x0080FF)
     
-    embed.add_field(name="1팀",value=f"[TANKER]김범주\n [BRAIN]이민형\n [SUB TANKER]유지호\n [NORMAL]김홍열",inline=False)
-    embed.add_field(name="2팀",value=f"[TANKER]김건범\n [BRAIN]이상백\n [SUB TANKER]박광현\n [NORMAL]이승혁",inline=False)
+    embed.add_field(name="콜러팀",value=f"[TANKER]김범주\n [BRAIN]이상백\n [SUB TANKER]유지호\n [SUB TANKER]박광현",inline=False)
+    embed.add_field(name="마을팀",value=f"[TANKER]김건범\n [BRAIN]이민형\n [NORMAL]김홍열\n [NORMAL]이승혁",inline=False)
     await ctx.send(embed = embed)
     return
     
 @client.command()
-async def 집합(ctx):
-    await ctx.channel.send ("모여라 !! @everyone")
+async def 집합(ctx, message):
+    await ctx.channel.send ("모여라 !! @everyone,\n" + message)
     return ctx
+
+
 
 
 client.run(TOKEN)
